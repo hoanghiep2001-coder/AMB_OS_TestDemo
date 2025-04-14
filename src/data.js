66a6e0389dfa146828a1e4f66b5b578bde9b430c
HiepDH <hiepdh@ieccorp.vn>
2025-04-14 23:16:53 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,122 @@
+
+
+const BestPA = [
+
+    {
+        timeline: "List:",
+        data: [
+            {
+                path: "/OutSource/GoodSort_Demo_PA_03.html",
+                name: "GoodSort_Demo_PA_03",
+                dev: "",
+            },
+            {
+                path: "/OutSource/GoodSort_Demo_PA_02.html",
+                name: "GoodSort_Demo_PA_02",
+                dev: "",
+            },
+            // {
+            //     path: "/PAForCV/WinterWolf_IEC_Games_PA_Test.html",
+            //     name: "WinterWolf_IEC_Games_PA_Test",
+            //     dev: "",
+            //     impression: "327.194",
+            //     CTR: "54.72%",
+            //     CVR: "9.64%",
+            //     IVR: "5.24%",
+            //     net: "Mintegral",
+            //     updatedDate: "7 - 2024"
+            // },
+            {
+                path: "/PAForCV/Food_Swept_Playable_Fix_Pyramid.html",
+                name: "Food_Swept_Playable_Fix_Pyramid",
+                dev: "",
+                impression: "327.194",
+                CTR: "54.72%",
+                CVR: "9.64%",
+                IVR: "5.24%",
+                net: "Mintegral",
+                updatedDate: "7 - 2024"
+            },
+            {
+                path: "/PAForCV/Food_Swept_Playable_Fix_Circle.html",
+                name: "Food_Swept_Playable_Fix_Circle",
+                dev: "",
+                impression: "327.194",
+                CTR: "54.72%",
+                CVR: "9.64%",
+                IVR: "5.24%",
+                net: "Mintegral",
+                updatedDate: "7 - 2024"
+            },
+            // {
+            //     path: "/PAForCV/Unimob_PA_test.html",
+            //     name: "Unimob_PA_test",
+            //     dev: "",
+            //     impression: "327.194",
+            //     CTR: "54.72%",
+            //     CVR: "9.64%",
+            //     IVR: "5.24%",
+            //     net: "Mintegral",
+            //     updatedDate: "7 - 2024"
+            // },
+        ]
+    },
+
+    // {
+    //     timeline: "Tháng 7:",
+    //     data: [
+    //         {
+    //             path: "/BestPA/Month7_2024/BattleDoll_Playable_MTG_Op1_GP_Click_PkMode.html",
+    //             name: "BattleDoll_Playable_MTG_Op1_GP_Click_PkMode",
+    //             dev: "Quân",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable.html",
+    //             name: "Preview_Billard_Puzzle_TVC_Android_Playable_OP12_Drag_GP_LongFlowBasicTable",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker.html",
+    //             name: "Preview_MakeoverASMR_Playable_Op35_Drag_FGP_BackMaker",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow.html",
+    //             name: "Preview_MergeRobo_Playable_Unity_OP5_GP_FullFlow",
+    //             dev: "Phong",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_1_GP_Level12.html",
+    //             name: "Satisday_Playable_Applovin_Op3_1_GP_Level12",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/Satisday_Playable_Applovin_Op3_GP_Level12.html",
+    //             name: "Satisday_Playable_Applovin_Op3_GP_Level12",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon.html",
+    //             name: "StickCarft_Playable_Applovin_Op6_GP_ChooseWeapon",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku.html",
+    //             name: "StickCraft_Playable_Applovin_Op3_Click_GP_StickSonGoku",
+    //             dev: "Hiệp",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP4_Drag_GP_Fight.html",
+    //             name: "StickCraft_Playable_Applovin_OP4_Drag_GP_Fight",
+    //             dev: "Quân",
+    //         },
+    //         {
+    //             path: "/BestPA/Month7_2024/StickCraft_Playable_Applovin_OP5_Click_FGP_Fight.html",
+    //             name: "StickCraft_Playable_Applovin_OP5_Click_FGP_Fight",
+    //             dev: "Quân",
+    //         },
+    //     ]
+    // },
+]
+
+export default BestPA;
